4ff1e53e976b3f666f4f5a0e1262272b23842009
tuinderj <joshua.tuinder@gmail.com>
2023-05-15 13:39:13 -0600

    removed functions

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,6 +3,7 @@ import logo from '../../assets/leasing-logo.png';
 import { FaBars } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { NavbarProps } from '../types';
+import auth from '../../utils/auth';
 
 export default function Navbar({ vehicleInformationState, vehicleIsInDatabase }: NavbarProps) {
   const handleToggleMenu = () => {
@@ -27,21 +28,25 @@ export default function Navbar({ vehicleInformationState, vehicleIsInDatabase }:
         <Link to={'/'}>
           <Logo src={logo} alt='Logo' />
         </Link>
-        <MenuIcon onClick={handleToggleMenu}>
-          <FaBars />
-        </MenuIcon>
+        {auth.loggedIn() ? (
+          <MenuIcon onClick={handleToggleMenu}>
+            <FaBars />
+          </MenuIcon>
+        ) : (
+          <></>
+        )}
       </LogoContainer>
       <LinksContainer id='links-container'>
-        <NavLink to={'/new_vehicle'}>New Vehicle</NavLink>
-        {vehicleIsInDatabase ? <NavLink to={'/edit_vehicle'}>Edit Vehicle</NavLink> : <></>}
-        {vehicleIsInDatabase ? (
+        {auth.loggedIn() ? <NavLink to={'/new_vehicle'}>New Vehicle</NavLink> : <></>}
+        {auth.loggedIn() && vehicleIsInDatabase ? <NavLink to={'/edit_vehicle'}>Edit Vehicle</NavLink> : <></>}
+        {auth.loggedIn() && vehicleIsInDatabase ? (
           <RemoveVehicleButton type='button' onClick={removeVehicle}>
             Remove Vehicle
           </RemoveVehicleButton>
         ) : (
           <></>
         )}
-        <LogButton>Logout</LogButton>
+        {auth.loggedIn() ? <LogButton>Logout</LogButton> : <LogButton>Login</LogButton>}
       </LinksContainer>
     </OuterContainer>
   );
---
+++ src/utils/auth.ts
@@ -0,0 +1,45 @@
+import decode from 'jwt-decode';
+
+class AuthService {
+  getProfile(): string {
+    return decode(this.getToken() || 'Not Logged In');
+  }
+
+  loggedIn(): boolean {
+    // Checks if there is a saved token and it's still valid
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
+
+  isTokenExpired(token: string): boolean {
+    try {
+      const decoded: any = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return true;
+    }
+  }
+
+  getToken(): string {
+    // Retrieves the user token from localStorage
+    return localStorage.getItem('id_token') || '';
+  }
+
+  login(idToken: string): void {
+    // Saves user token to localStorage
+    localStorage.setItem('id_token', idToken);
+
+    window.location.assign('/');
+  }
+
+  logout(): void {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem('id_token');
+    // this will reload the page and reset the state of the application
+    window.location.assign('/');
+  }
+}
+
+export default new AuthService();
